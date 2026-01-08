4ae62c4392f5e0fe8a333a4dca9d85ce88a040ed
Jinbungkun <Jinbungkun@gmail.com>
2026-01-09 01:54:19 +0900

    임시 아이콘 추가

--- sw.js
+++ sw.js
@@ -1,3 +1,24 @@
-self.addEventListener('fetch', function(event) {
-  // 브라우저가 앱으로 인식하게 하기 위한 빈 서비스 워커입니다.
+const CACHE_NAME = 'checkin-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './ui.js',
+  './manifest.json',
+  './icon-512.png'
+];
+
+// 설치 시 자원 캐싱
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+// 네트워크 우선 전략 (데이터 업데이트를 위해)
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
 });
